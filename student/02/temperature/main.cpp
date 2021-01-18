2150bd1b2b7c99804b1cdb63a4be9755eb20d801
Onni Merilä <onni.merila@tuni.fi>
2021-01-18 20:28:51 +0200

    2.2.2 v1.1

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -5,6 +5,14 @@ using namespace std;
 int main()
 {
     cout << "Enter a temperature: ";
+    double temperature;
+    cin >> temperature;
+    double temp_cels_to_fahren = 1.8*temperature + 32;
+    double temp_fahren_to_cels = (temperature-32)/1.8;
+    cout << temperature << " degrees Celsius is " << temp_cels_to_fahren
+         << " degrees Fahrenheit " << endl;
+    cout << temperature << " degrees Fahrenheit is " << temp_fahren_to_cels
+         << " degrees Celsius " << endl;
 
     // Write your code here
 
