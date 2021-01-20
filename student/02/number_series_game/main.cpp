07c7452e6988c495cf3f12eaa0d99abece8506ae
Onni Merilä <onni.merila@tuni.fi>
2021-01-20 16:00:12 +0200

    Lisatty kaikki toiminnallisuudet

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -4,4 +4,19 @@
 int main()
 {
     std::cout << "How many numbers would you like to have? ";
+    int numbers = 10;
+    std::cin >> numbers;
+
+    for (int i = 1;i <= numbers; ++i) {
+        if (i % 3 == 0 and i % 7 == 0){
+            std::cout << "zip boing" << std::endl;
+        } else if (i % 3 == 0) {
+            std::cout << "zip" << std::endl;
+        } else if (i % 7 == 0) {
+            std::cout << "boing" << std::endl;
+        } else {
+            std::cout << i << std::endl;
+        }
+    }
+
 }
