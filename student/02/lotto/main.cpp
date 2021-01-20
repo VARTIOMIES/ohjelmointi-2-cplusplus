caae1323cffe0389add0a61c1b763749ff1194a8
Onni Merilä <onni.merila@tuni.fi>
2021-01-21 00:40:13 +0200

    Lisätty main ja korjattu kertoma funktiota muuttamalla int unsigned long inteiksi

--- student/02/lotto/main.cpp
+++ student/02/lotto/main.cpp
@@ -2,21 +2,40 @@
 
 using namespace std;
 
-int kertoma(int luku){
-    int luvun_kertoma = 0;
+unsigned long int kertoma(int luku){
+    unsigned long int luvun_kertoma = 1;
     for (int i = 1; i <= luku;i++){
         luvun_kertoma *= i;
     }
     return luvun_kertoma;
 }
 int lottorivien_maara(int kaikki_pallot, int arvotut_pallot){
-    int osoittaja = kertoma(kaikki_pallot);
-    int nimittaja = kertoma(kaikki_pallot - arvotut_pallot) * kertoma(arvotut_pallot);
-    return osoittaja / nimittaja;
-
+    unsigned long int osoittaja = kertoma(kaikki_pallot);
+    unsigned long int nimittaja = kertoma(kaikki_pallot - arvotut_pallot) * kertoma(arvotut_pallot);
+    unsigned long int maara = osoittaja/nimittaja;
+    return maara;
+}
+bool positiiviset(int luku1, int luku2){
+    if (luku1 >= 0 and luku2 >=0){
+        return true;
+    } else {
+        return false;
+    }
 }
 int main()
 {
-    cout << "Hello World!" << endl;
+    cout << "Enter the total number of lottery balls: ";
+    int pallot = 0;
+    cin >> pallot;
+    cout << "Enter the number of drawn balls: ";
+    int arvottavat_pallot = 0;
+    cin >> arvottavat_pallot;
+    if (not positiiviset(pallot, arvottavat_pallot)){
+        cout << "The number of balls must be a positive number." << endl;
+    } else {
+        cout << "The probability of guessing all " << arvottavat_pallot << " balls correctly is 1/" << lottorivien_maara(pallot, arvottavat_pallot) << endl;
+    }
+
+
     return 0;
 }
