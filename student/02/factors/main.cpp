824fe29768e2c4c6ac6704ed04d48a82e0124490
Onni Merilä <onni.merila@tuni.fi>
2021-01-20 17:25:18 +0200

    2.3.2 v1.0 Ekat toiminnallisuudet

--- student/02/factors/main.cpp
+++ student/02/factors/main.cpp
@@ -5,6 +5,25 @@ using namespace std;
 int main()
 {
     cout << "Enter a positive number: ";
-
+    int number = 0;
+    cin >> number;
+    if (number <= 0){
+        cout << "Only positive numbers accepted" << endl;
+    } else {
+        int jako = 1;
+        int tekija1 = number;
+        int tekija2 = 1;
+        for (int i = 1; i <= number; ++i){
+            // Tarkistetaan onko luku jaollinen i:llä
+            if (number % i == 0){
+                jako = number/i;
+                if (abs(jako-i) < abs(tekija1-tekija2)){
+                    tekija1 = i;
+                    tekija2 = jako;
+                }
+            }
+        }
+        cout << tekija1 << tekija2 << endl;
+    }
     return 0;
 }
