c6bea5a2710e76a067c4745690f3022372f451d4
Onni Merilä <onni.merila@tuni.fi>
2021-01-26 21:06:38 +0200

    2.6.1 Muutettu edellistä ASCII tarkastelua ja luotu kaksi eri tarkastelufunktiota, joiden toiminnallisuus testattu. Main myös toimii tähän asti oikein

--- student/02/encryption/main.cpp
+++ student/02/encryption/main.cpp
@@ -4,19 +4,46 @@
 
 using namespace std;
 
-bool lower_az_ASCII(string string) {
+bool only_az_ASCII(string string) {
     int pituus = string.length();
     for (int i = 0; i < pituus; ++i){
-        char merkki = string[i];
-        int merkin_ASCII = static_cast<int>(merkki);
-        if (not islower(merkki)){
-            return false;
-        } else if (merkin_ASCII < 97 or merkin_ASCII > 122){
+        int merkin_ASCII = static_cast<int>(string[i]);
+        if (merkin_ASCII < 97 or merkin_ASCII > 122){
             return false;
         }
-
     }
     return true;
+}
+
+bool all_az_ASCII(string string) {
+    int pituus = string.length();
+    // Käydään läpi aakkoset
+    for (int j = 97; j <= 122; ++j){
+        bool kirjain_sanassa = false;
+        // Käydään läpi string
+        for (int i = 0; i < pituus; ++i){
+            char merkki = string[i];
+            int merkin_ASCII = static_cast<int>(merkki);
+            // Tarkastetaan, onko merkki ja tämän hetkinen aakkonen samat
+            if (merkin_ASCII == j){
+                // Jos on, niin poistutaan sanan läpikäymisestä, koska tämä aakkonen
+                //on ainakin sanassa
+                kirjain_sanassa = true;
+                break;
+            }
+        }
+        // Jos sana on käyty kokonaan läpi, ja kirjainta ei ole sanassa
+        // niin kirjain_sanassa muuttuja on false, jolloin tässä vaiheessa voidaan
+        // todeta, että kaikki aakkoset eivät ole sanassa.
+        if (not kirjain_sanassa){
+            return false;
+        }
+        // Siirrytään seuraavaan aakkoseen, mikäli edellinen aakkonen ainakin oli sanassa
+    }
+    // Kaikki aakkoset on käyty läpi, ja funktiosta ei olla poistuttu, joten kaikki
+    // aakkoset ovat sanassa. Palautetaan totuusarvo true
+    return true;
+
 }
 
 int main()
@@ -28,7 +55,12 @@ int main()
     if (encryption_key.length() != 26){
         cout << "Error! The encryption key must contain 26 characters." << endl;
         return 0;
-    } else if (lower_az_ASCII(encryption_key))
+    } else if (not only_az_ASCII(encryption_key)){
+        cout << "Error! The encryption key must contain only lower case characters." << endl;
+    } else if (not all_az_ASCII(encryption_key)){
+        cout << "Error! The encryption key must contain all alphabets a-z" << endl;
+    }
+
 
     return 0;
 }
