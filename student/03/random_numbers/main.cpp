11f7ae4fedf14910876385e7bb932c65ffe4e097
Onni Merilä <onni.merila@tuni.fi>
2021-01-27 21:27:44 +0200

    3.2.2 valmis

--- student/03/random_numbers/main.cpp
+++ student/03/random_numbers/main.cpp
@@ -6,7 +6,18 @@ using namespace std;
 
 void produce_random_numbers(unsigned int lower, unsigned int upper)
 {
-    // Implement your function here
+    int seed = 0;
+    cout << "Enter a seed value: ";
+    cin >> seed;
+    char command = 'c';
+    default_random_engine gen(seed);
+    uniform_int_distribution<int> distr(lower, upper);
+    while (command == 'c') {
+
+        cout << "Your drawn random number is " << distr(gen) << endl
+             << "Press c to continue or q to quit: ";
+        cin >> command;
+    }
 }
 
 int main()
