aeef8a14e9ff8b12875ac5ca538809cc910d92ff
Onni Merilä <onni.merila@tuni.fi>
2021-01-30 18:38:04 +0200

    Kirjasto, valmis

--- student/03/simple_library/book.cpp
+++ student/03/simple_library/book.cpp
@@ -2,9 +2,9 @@
 #include <iostream>
 #include "date.hh"
 
-Book::Book()
+Book::Book(std::string kirjailija,std::string kirjan_nimi):
+    kirjailija_(kirjailija),name_(kirjan_nimi)
 {
-
 }
 
 void Book::print() const
@@ -25,15 +25,28 @@ void Book::print() const
 
 void Book::loan(Date date)
 {
+    if (not available_){
+        std::cout << "Already loaned: cannot be loaned" << std::endl;
+    }
+    else{
+        loan_day_ = date;
+        date.advance(28);
+        due_day_ = date;
+        available_ = false;
+    }
 
 }
 
 void Book::renew()
 {
-
+    if (available_){
+        std::cout << "Not loaned: cannot be renewed" << std::endl;
+    } else{
+        due_day_.advance(28);
+    }
 }
 
 void Book::give_back()
 {
-
+    available_ = true;
 }
