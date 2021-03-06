cb92ddee7a19be0a23e6c0659e49fe3fa200bc68
Onni Merilä <onni.merila@tuni.fi>
2021-03-06 14:44:37 +0200

    6.1.3 Toimii isompi jaollisuus

--- student/06/vertical/main.cpp
+++ student/06/vertical/main.cpp
@@ -11,7 +11,15 @@ void print_vertical(unsigned int num)
     // Do not remove RECURSIVE_FUNC declaration, it's necessary for automatic testing to work
     // ------------
     unsigned int jakaja;
-    if (num > 100000)
+    if (num > 10000000)
+    {
+        jakaja = 10000000;
+    }
+    else if (num > 1000000)
+    {
+        jakaja = 1000000;
+    }
+    else if (num > 100000)
     {
         jakaja = 100000;
     }
