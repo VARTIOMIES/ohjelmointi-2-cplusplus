a04059848ad766e38aec50c31a9cfec42ad5ce60
Onni Merilä <onni.merila@tuni.fi>
2021-03-06 14:42:10 +0200

    6.1.3 Toimii 100000 jaollisuus

--- student/06/vertical/main.cpp
+++ student/06/vertical/main.cpp
@@ -10,8 +10,42 @@ void print_vertical(unsigned int num)
     RECURSIVE_FUNC
     // Do not remove RECURSIVE_FUNC declaration, it's necessary for automatic testing to work
     // ------------
-
-
+    unsigned int jakaja;
+    if (num > 100000)
+    {
+        jakaja = 100000;
+    }
+    else if (num >10000)
+    {
+        jakaja = 10000;
+    }
+    else if(num>1000)
+    {
+        jakaja = 1000;
+    }
+    else if (num>100)
+    {
+        jakaja = 100;
+    }
+    else if (num>10)
+    {
+        jakaja = 10;
+    }
+    else
+    {
+        std::cout << num<< std::endl;
+        return;
+    }
+    // Tulostetaan numero ja vähennetään siitä, jotta voidaan tulostaa seuraavat
+    unsigned int eka_num = num/jakaja;
+    std::cout << eka_num<< std::endl;
+    //Jatketaan jakojäännöksellä
+    int uusi_num  = num%jakaja;
+    if (uusi_num/(jakaja/10)==0)
+    {
+        std::cout<<0<<std::endl;
+    }
+    print_vertical(num % jakaja);
     // Add your implementation here
 }
 
