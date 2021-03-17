92a4b92dc5366f5d10ecafb870270129dde979c6
Onni Merilä <onni.merila@tuni.fi>
2021-03-17 20:34:24 +0200

    8.3.2 fixed v4

--- student/08/student_register/main.cpp
+++ student/08/student_register/main.cpp
@@ -85,7 +85,7 @@ void change_number(Student* s){
     std::cout << "Enter a new phone number: ";
     std::string input = "";
     getline(std::cin,input);
-    std::cout<<std::endl;
+    std::cout<<"" <<std::endl;
     if (is_valid_phone_number(input)) {
         s->phone_number = input;
     }
