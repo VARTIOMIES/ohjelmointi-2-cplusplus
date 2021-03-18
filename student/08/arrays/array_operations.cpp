6936ce41a892a2292779ee29ee72f503a3cacfb1
Onni Merilä <onni.merila@tuni.fi>
2021-03-18 12:57:13 +0200

    8.4.1

--- student/08/arrays/array_operations.cpp
+++ student/08/arrays/array_operations.cpp
@@ -1,1 +1,52 @@
 #include "array_operations.hh"
+
+int greatest_v1(int *itemptr, int size)
+{
+    int max=0;
+    for (int i =0;i<size;i++,itemptr++)
+    {
+        if (*itemptr>max)
+        {
+            max = *itemptr;
+        }
+    }
+    return max;
+}
+
+int greatest_v2(int *itemptr, int *endptr)
+{
+    int max = 0;
+    for (int* position = itemptr ;*position<*endptr; position++)
+    {
+        if (*position>max)
+        {
+            max=*position;
+        }
+    }
+    return max;
+}
+
+void copy(int *itemptr, int *endptr, int *targetptr)
+{
+    while(itemptr<endptr)
+    {
+        *targetptr = *itemptr;
+        itemptr++;
+        targetptr++;
+    }
+}
+
+
+void reverse(int *leftptr, int *rightptr)
+{
+    rightptr--;
+    while (leftptr<rightptr)
+    {
+        int temp = 0;
+        temp = *rightptr;
+        *rightptr = *leftptr;
+        *leftptr=temp;
+        leftptr++;
+        rightptr--;
+    }
+}
