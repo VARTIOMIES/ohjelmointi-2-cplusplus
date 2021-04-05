0028790518b008b071fbab5b4c011e7a56b4944c
Onni Merilä <onni.merila@tuni.fi>
2021-04-05 20:42:24 +0300

    10.3.1 Syöpänen vittusaatana

--- student/10/reverse/cards.cpp
+++ student/10/reverse/cards.cpp
@@ -24,6 +24,38 @@ void Cards::print(std::ostream& s) {
    }
 }
 
+bool Cards::remove(int& id)
+{
+    if (top_!=nullptr)
+    {
+        id = top_->data;
+        top_ = top_->next;
+        return true;
+    }
+   return false;
+}
+
+void Cards::reverse()
+{
+    std::shared_ptr<Card_data> current = top_;
+    std::shared_ptr<Card_data> actual_next = current->next;
+    std::shared_ptr<Card_data> next_of_actual_next = actual_next;
+    top_->next = nullptr;
+    // "Käännetään nuolein suunnat"
+    while (next_of_actual_next != nullptr)
+    {
+        next_of_actual_next = actual_next->next;
+        // Kääntö
+        actual_next->next = current;
+
+        // Apu osoittimien siirto
+        current = actual_next;
+        actual_next = next_of_actual_next;
+    }
+    top_ = current;
+
+}
+
 // Tip for writing code more efficiently:
 // Do not write the stubs of the methods remove and reverse by yourself here,
 // but open the file cards.hh and click the declaration of the method
